dac50444c87b8a8cf848625fed6547298852b67d
IrynaAn <airyna1@mail.ru>
2022-04-28 08:33:31 +0300

    feat: add solutions

--- src/file-names.js
+++ src/file-names.js
@@ -1,7 +1,7 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * There's a list of file, since two files cannot have equal names,
+ * There's a list of file, since two files cannot have equal names,fil
  * the one which comes later will have a suffix (k),
  * where k is the smallest integer such that the found name is not used yet.
  *
@@ -15,9 +15,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+   for (let i=0; i<=names.length-1; i++) {
+     let n=1;
+     for (let j=i+1; j<names.length; j++){if (names[i]===names[j]){names[j]+=`(${n})`;n++}}
+   }
+  return names;
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  num = String(n).split('');
+  sum = 0;
+  while (num.length > 1) {
+    for (let i = 0; i < num.length; i++) {sum += +num[i];}
+    num = String(sum).split('');
+    if (num.length > 1) {sum = 0;}
+  }
+  return sum;
+
 }
 
 module.exports = {
