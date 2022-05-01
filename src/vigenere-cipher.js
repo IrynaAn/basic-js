00186d00979c77d23cf26c4166cf9cfab35d1be6
IrynaAn <airyna1@mail.ru>
2022-05-01 21:48:37 +0300

    feat: add last solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  
-  encrypt() {
-    
+  constructor(t) {
+    if (t == false) { this.reverse = true;}
   }
 
-  decrypt() {
-  
+  encrypt(str1, str2) {
+    if (!str1||!str2) {throw new Error("Incorrect arguments!");}
+    let m = str1.toUpperCase(), k = str2.toUpperCase();
+    let mCode = '';
+    let i = 0;
+    for (let char of m) {
+        if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
+          mCode += String.fromCharCode(((char.charCodeAt(0) - 65 + k.charCodeAt(i) - 65) % 26) + 65); i++;
+          if (i === k.length) i = 0;
+        } else {mCode += char;}
+    }
+    if (this.reverse) {mCode = mCode.split("").reverse().join("");} 
+    return mCode;
   }
+
+  decrypt(str1, str2) {
+    if (!str1||!str2) {throw new Error("Incorrect arguments!");}
+    let m = str1.toUpperCase(), k = str2.toUpperCase();
+    let mCode = "";
+    let i = 0;
+    for (let char of m) {
+      if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
+        mCode += String.fromCharCode(((char.charCodeAt(0) - k.charCodeAt(i) + 26) % 26) + 65); i++;
+        if (i === k.length) i = 0;
+      } else {mCode += char;}
+    }
+    if (this.reverse) {mCode = mCode.split("").reverse().join("");}
+    return mCode;
+  } 
 }
-
 module.exports = {
   VigenereCipheringMachine
 };
